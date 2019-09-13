49a6f19be2901d868b211233bbb2f9ddbf7adf5f
Keijiro Takahashi <keijiro@gmail.com>
2019-09-13 23:51:22 +0900

    hah

--- Assets/Stage/Bars.hlsl
+++
@@ -1,9 +0,0 @@
-#include "SimplexNoise2D.hlsl"
-
-void Bars_float(float2 UV, float Frequency, float Time, float Threshold, out float Intensity)
-{
-    float2 p1 = float2(UV.y * Frequency, Time);
-    float2 p2 = p1 * float2(3.734, -2.7134);
-
-    Intensity = abs(snoise(p1) + snoise(p2)) < Threshold;
-}
