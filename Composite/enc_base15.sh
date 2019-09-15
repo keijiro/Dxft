c2d8f0c811257f8c1a18e66b70e2251ef71cd2f5
Keijiro Takahashi <keijiro@gmail.com>
2019-09-15 22:55:42 +0900

    lots of things

--- Composite/enc_base15.sh
+++ Composite/enc_base15.sh
@@ -1,33 +1,49 @@
 #!/bin/sh
 
-BASE="Source/Base15.mp4"
-MASK="Source/Mask.png"
-
-MUX_FILTER="\
-[0:v] format=rgba [base]; \
-[1:v] format=rgba [title]; \
-[2:v] format=rgba [mask]; \
-[base][title] blend=all_mode=addition [v]; \
-[v][mask] blend=all_mode=multiply, pad=height=2160:y=360:color=black" 
-
 composite()
 {
-    VIDEO="Source/Title$1.mp4"
+    INDEX=`printf "%02d" $(( $1 + 1 ))`
+    TITLE_POS=$(( $1 * 4 ))
+
+    BASE="Source/Base15.mov"
+    MASK="Source/Mask.png"
+
+    BLANK2S="color=black:duration=2:size=3840x1440"
+
+    TITLE="Source/Titles.mov"
     AUDIO="../Assets/Audio/RE_15sec mixB_0911.wav"
-    RENDER="Render/Bumper_Main_$1.mov"
-    PREVIEW="Render/Bumper_Main_$1.mp4"
+    RENDER="Render/Bumper15_Main_$INDEX.mov"
+    PREVIEW="Render/Bumper15_Main_$INDEX.mp4"
+
+    MUX_FILTER="
+[0:v] format=rgba [base];
+[1:v] format=rgba [titles];
+[2:v] format=rgba [mask];
+[3:v] format=rgba,setsar=0:1 [blank];
+[base] split [base1][base2];
+[base1] trim=duration=9 [base1t];
+[base2] trim=start=9, setpts=PTS-STARTPTS [base2t];
+[titles] trim=start=$TITLE_POS:duration=4, setpts=PTS-STARTPTS [title];
+[base2t][title] blend=all_mode=addition [base2x];
+[base1t][base2x][blank] concat=3 [cat];
+[cat][mask] blend=all_mode=multiply, pad=height=2160:y=360:color=black" 
 
     echo "$RENDER"
 
-    ffmpeg -y \
-        -i "$BASE" -i "$VIDEO" -i "$MASK" \
-        -r 59.94 -filter_complex "$MUX_FILTER" \
-        -c:v hap -an "$RENDER"
+    ffmpeg -y -v 0 \
+        -i "$BASE" -i "$TITLE" -i "$MASK" -f lavfi -i "$BLANK2S" \
+        -filter_complex "$MUX_FILTER" \
+        -r 59.94 -c:v hap -an "$RENDER"
 
     echo "$PREVIEW"
 
-    ffmpeg -y -i "$RENDER" -i "$AUDIO" -pix_fmt yuv420p "$PREVIEW"
+    ffmpeg -y -v 0 -i "$RENDER" -i "$AUDIO" -pix_fmt yuv420p "$PREVIEW"
 }
 
-composite 001 &
+composite 8 &
+composite 9 &
+composite 10 &
+composite 11 &
+composite 12 &
+composite 13 &
 wait
